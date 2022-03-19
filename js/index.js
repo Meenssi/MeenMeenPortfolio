9721088545a0deb10d4bd851b0f10cc87a066c61
Meenssi <fumikoe439@gmail.com>
2022-03-19 13:49:47 +0700

    coding 1
    เสร็จแล้ว

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function Func_1() {
+    document.getElementById("text-1").innerHTML = "<hr>" + "Name : Sirapatson Imkrajang" + "<br>" + "Location : Thailand" + "<hr>" ;
+    
+}
+
+function Func_2() {
+    document.getElementById("text-1").innerHTML =  "<hr>" + "I am a student at Bangkok University." + "<br> <br>" + "I'm studying JavaScript Programming" + "<br>" + "with Visual Studio Code." + "<hr>";
+}
+
+
